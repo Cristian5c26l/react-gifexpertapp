2932784ea8cd5e6945fa8ce2a981553c6e6db5b4
cristian5464@outlook.com <cristianhernandezresendiz@gmail.com>
2021-07-07 14:33:01 -0500

    docs added Gif Expert App React

---
+++ docs/precache-manifest.30927990d0982f7e1555e7a648b2e6a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e250e77dd28bf49f7a159a59b1fd91ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb3895bc2a3a1aec6b2b",
+    "url": "/static/css/main.120b4e0a.chunk.css"
+  },
+  {
+    "revision": "21128ddbf1ff63946978",
+    "url": "/static/js/2.31a10122.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.31a10122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb3895bc2a3a1aec6b2b",
+    "url": "/static/js/main.81040fdd.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
